8d509e8b6973766f0c61007e52d163391409f9e9
Geebe92 <b.karnas92@gmail.com>
2023-04-09 19:09:40 +0200

    Poprawa ZadanieRepository: zmiana paremetrów query

--- src/main/java/com/project/rest/repository/ZadaniaRepository.java
+++ src/main/java/com/project/rest/repository/ZadaniaRepository.java
@@ -10,8 +10,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ZadaniaRepository extends JpaRepository<Zadanie, Integer> {
-    @Query("SELECT z FROM Zadanie z WHERE z.projekt.projektId = :projekt")
+    @Query("SELECT z FROM Zadanie z WHERE z.projekt.projektId = :projektId")
     Page<Zadanie> findZadaniaProjektu(@Param("projektId") Integer projektId, Pageable pageable);
-    @Query("SELECT z FROM Zadanie z WHERE z.projekt.projektId = :projekt")
+    @Query("SELECT z FROM Zadanie z WHERE z.projekt.projektId = :projektId")
     List<Zadanie> findZadaniaProjektu(@Param("projektId") Integer projektId);
 }
